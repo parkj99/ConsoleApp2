33bef2699b6807f8dab98e320b4fb498c5365327
jpark326 <park.joseph@outlook.com>
2021-09-12 17:27:21 -0700

    verifying whether git changes properly

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hellogit");
+            Console.WriteLine("master branch");
         }
     }
 }
